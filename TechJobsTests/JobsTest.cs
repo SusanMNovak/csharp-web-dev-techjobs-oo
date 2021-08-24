177061312cb64457a06f05e7a9e2d193c28bbc9f
susan.novak@yahoo.com <susan.novak@yahoo.com>
2021-08-23 21:06:49 -0500

    Started Unit Testing

---
+++ TechJobsTests/JobsTest.cs
@@ -0,0 +1,29 @@
+﻿using TechJobsOO;
+
+namespace TechJobsTests
+{
+    class JobsTest
+    {
+        Job job1;
+        Job job2;
+        Job job3;
+        Job job4;
+
+        [TestInitialize]
+        public void CreateJobObjects()
+        {
+            job1 = new Job();
+            job2 = new Job();
+            job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        }
+
+        [TestMethod]
+        public void TestSettingJobId()
+        {
+            Assert.IsTrue(job1.Id != job2.Id && (job1.Id + 1) == job2.Id);
+        }
+
+
+    }
+}
